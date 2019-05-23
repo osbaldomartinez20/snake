67103018bb8d63fadc45337366f1fdfe8141adf2
osbaldomartinez20 <omartin6@mail.sfsu.edu>
2019-05-23 11:31:49 -0700

    finished movement methods in js version of game.

--- Javascript/snake.js
+++ Javascript/snake.js
@@ -29,18 +29,67 @@ var INPUT_RIGHT = "D";
 var INPUT_LEFT = "A";
 
 //function that initializes the game with a board of height h and width w.
-var gameInit = function (h=10, w=20) {
+var gameInit = function (h = 10, w = 20) {
     HEIGHT = h;
     WIDTH = w;
-    snakeInit(SNAKE_HEAD);
+    snakeInit(SNAKE_HEAD, UP);
 }
 
 //initializes the snake with a coordinate coor.
-var snakeInit = function (coor) {
-    SNAKE_BODY.push(coor);
+var snakeInit = function (coor, dir) {
+    DIRECTION = [...dir];
+    SNAKE_BODY.push([...coor]);
 }
 
-//initializes the apple with a coordinate coor.
-var appleInit = function (coor) {
-    APPLE_LOCATION = [...coor];
+//function returns the cooordinate of the snake's head
+var head = function () {
+    return [...SNAKE_HEAD];
+}
+
+//function gives new location for apple
+var appleLocation = function () {
+    while (true) {
+        APPLE_LOCATION[0] = Math.floor(Math.random() * HEIGHT);
+        APPLE_LOCATION[1] = Math.floor(Math.random() * WIDTH);
+        if (SNAKE_BODY.indexOf(APPLE_LOCATION) < 0) {
+            break;
+        }
+    }
+}
+
+//sets the direction
+var setDirection = function (dir) {
+    DIRECTION = [...dir];
+}
+
+//extends the snake's body with coordinate position
+var extendBody = function (position) {
+    SNAKE_BODY.push([...position]);
+}
+
+//function that calculates the new position of the snake head
+var nextPosition = function (position, step) {
+    var new_pos = [mod(position[0] + step[0], HEIGHT), mod(position[0] + step[0], WIDTH)];
+    return new_pos;
+}
+
+//this function makes the snake take a step
+var takeStep = function (position) {
+    var temp = [...SNAKE_BODY];
+    for (var i = 0; i < temp.length; i++) {
+        if (i == temp.length-1) {
+            SNAKE_BODY[i] = position;
+        } else {
+            SNAKE_BODY[i] = temp[i+1];
+        }
+    }
+}
+
+
+
+
+
+//this solves the negative mod problem
+var mod = function (x, m) {
+    return (x % m + m) % m;
 }
